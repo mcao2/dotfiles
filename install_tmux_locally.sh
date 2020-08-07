c482ca5420b6e580e767360caa7ab4e59d5b27a3
mcao2 <mcao2@andrew.cmu.edu>
2020-08-07 13:54:14 -0400

    Update tmux install script

--- install_tmux_locally.sh
+++ install_tmux_locally.sh
@@ -13,29 +13,6 @@ sudo apt-get install git make build-essential libssl-dev
 
 # Installing tmux locally
 
-# 1. visit: https://github.com/tmux/tmux
-# 2. create a local directory to install tmux
-# 3. download depencencies
-#     1. libevent
-#         1. visit: http://libevent.org/
-#         2. download the stable version
-#     2. ncurses
-#         1. visit: http://invisible-island.net/ncurses/
-#         2. download the stable version
-#     3. tmux
-#         1. visit: https://github.com/tmux/tmux/releases
-#         2. download the stable .tar.gz
-# 4. install all the dependencies first and last tmux by running following commands
-#
-# 	tar xvf [--file--.tar.gz]
-# 	cd [---file--]
-# 	./configure --prefix=$HOME/local CPPFLAGS="-P"
-# 	make
-# 	make install
-#
-# 5. `export PATH="$HOME/local/bin:$PATH"` into your bashrc file
-
-curr_path=$(pwd)
 rm -rf tmux_temp_installation_dir 2> /dev/null
 mkdir tmux_temp_installation_dir
 cd tmux_temp_installation_dir
@@ -56,35 +33,37 @@ ncursesurl="https://invisible-island.net/datafiles/release/ncurses.tar.gz"
 # read tmuxurl
 tmuxurl="https://github.com/tmux/tmux/releases/download/3.1b/tmux-3.1b.tar.gz"
 
-echo "Installing tmux into your ~/.local/bin/tmux"
-
-function installPackage {
-    # $1 holds tar.gz
-    rm -rf temp_tmux_install_lib_dir 2> /dev/null
-    mkdir temp_tmux_install_lib_dir
-    tar xvf $1 -C temp_tmux_install_lib_dir --strip-components=1
-    cd temp_tmux_install_lib_dir
-    ls
-    ./configure --prefix=$HOME/local CPPFLAGS="-P"
-    make -j 8
-    make install
-    cd ..
-}
+echo "Installing tmux into your ~/local/bin/tmux"
 
 wget $libeventurl -O libevent.tar.gz
 wget $ncursesurl -O ncurses.tar.gz
 wget $tmuxurl -O tmux.tar.gz
 
 echo "Installing libevent..."
-installPackage libevent.tar.gz
+rm -rf libevent_install && mkdir libevent_install
+tar -zxf libevent.tar.gz -C libevent_install --strip-components=1
+cd libevent_install
+./configure --prefix=$HOME/local --enable-shared
+make && make install
+cd .. && rm -rf libevent_install
 
 echo "Installing ncurses..."
-installPackage ncurses.tar.gz
+rm -rf ncurses_install && mkdir ncurses_install
+tar -zxf ncurses.tar.gz -C ncurses_install --strip-components=1
+cd ncurses_install
+./configure --prefix=$HOME/local --with-shared --with-termlib --enable-pc-files --with-pkg-config-libdir=$HOME/local/lib/pkgconfig
+make && make install
+cd .. && rm -rf ncurses_install
 
 echo "Installing tmux..."
-installPackage tmux.tar.gz
+rm -rf tmux_install && mkdir tmux_install
+tar -zxf tmux.tar.gz -C tmux_install --strip-components=1
+cd tmux_install
+PKG_CONFIG_PATH=$HOME/local/lib/pkgconfig ./configure --prefix=$HOME/local
+make && make install
+cd .. && rm -rf tmux_install
 
-cd $curr_path
+cd ..
 
 echo "Append following line to your .zshrc or .bashrc or .bash_profile file"
 echo export PATH="$HOME/local/bin:\$PATH"
