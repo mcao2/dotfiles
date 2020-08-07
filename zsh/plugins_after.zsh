4f90c7cada7451cde99b942d245f6eba9833b1d1
mcao2 <mcao2@andrew.cmu.edu>
2020-08-07 12:32:38 -0400

    Add autosuggestions plugin

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -35,3 +35,6 @@ fi
 if [[ "$(tput colors)" == "256" ]]; then
     eval $(dircolors =(cat ~/.shell/plugins/dircolors-solarized/dircolors.256dark ~/.shell/dircolors.extra))
 fi
+
+# Zsh autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
